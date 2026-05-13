68fcc21c24211a726af3c08d8e14d68c2b600c7c
davigabrielr <davi.roza@amf.edu.br>
2026-05-12 22:14:11 -0300

    Add Fibonacci sequence implementation
    Implement Fibonacci sequence printing for first 10 terms.

---
+++ exercicio3.py
@@ -0,0 +1,7 @@
+#vou pedir ajuda pro alan
+a, b = 0, 1
+print("Sequência de Fibonacci (10 primeiros termos):")
+for i in range(10):
+    print(a, end=" ")
+    a, b = b, a + b
+#esse cara é um genio
