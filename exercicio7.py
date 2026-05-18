801c462cfa84af2d61928ac3c0e2f4ad01a74a16
davigabrielr <davi.roza@amf.edu.br>
2026-05-18 15:39:40 -0300

    Refactor to use while loop for printing even numbers
    Replace for loop with a while loop to print even numbers.

--- exercicio7.py
+++ exercicio7.py
@@ -4,6 +4,9 @@ num2 = int(input("Digite o segundo número (maior): "))
 
 print("\nNúmeros pares entre", num1, "e", num2, ":")
 
-for i in range(num1, num2 + 1):
-    if i % 2 == 0:   
-        print(i)
+contador = num1
+
+while contador <= num2:
+    if contador % 2 == 0:
+        print(contador)
+    contador = contador + 1
